a9c6d31f96e8392a2d88094487afa32ff543339a
beneMati <benedettomatias1991@gmail.com>
2024-12-19 10:42:39 -0300

    server running message fix

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ db.sequelize.sync().then(() => {
   const server = http.createServer(app);
 
   server.listen(PORT, () => {
-    console.log(`Server is running! ${PORT}`);
+    console.log('Server is running!');
   });
 }).catch((error) => {
   console.error('Unable to connect to the database:', error);
